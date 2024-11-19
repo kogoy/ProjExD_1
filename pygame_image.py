a1f742b13022897e3906ffb661f3b2ef83eb4fa2
kogo <c0b230634c@edu.teu.ac.jp>
2024-11-19 15:56:50 +0900

    練習5:FPS200に

--- pygame_image.py
+++ pygame_image.py
@@ -19,9 +19,10 @@ def main():
 
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
