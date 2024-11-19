6bb81676988a2b14de9b303750ba65e2b2f1300c
kogo <c0b230634c@edu.teu.ac.jp>
2024-11-19 16:44:10 +0900

    練習7:るーぷかいしょう

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像Surfaceを作成する
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像Surafaceを作成する
     kk_img = pg.transform.flip(kk_img, True, False)  # こうかとんを左右反転させる
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800  # 練習6-2
-        screen.blit(bg_img, [-x, 0])  # screen Surfaceに背景画像Surfaceを貼り付ける
+        x = -(tmr%3200)
+        screen.blit(bg_img,  [x, 0])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img,  [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
         screen.blit(kk_img, [300, 200])  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
         pg.display.update()
         tmr += 1        
