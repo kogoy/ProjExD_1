780d72586981c9162f2bd03337570c25e4084136
kogo <c0b230634c@edu.teu.ac.jp>
2024-11-19 17:11:21 +0900

    練習8-1:rectしゅとく

--- pygame_image.py
+++ pygame_image.py
@@ -19,24 +19,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()
-        # print(key_lst[pg.K_UP], key_lst[pg.K_DOWN], 
-        #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
-        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
-            kk_rct.move_ip(+1, 0)
 
         x = -(tmr%3200)
         screen.blit(bg_img,  [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img,  [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(kk_img, kk_rct)  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
+        screen.blit(kk_img,[300,200]) 
         pg.display.update()
         tmr += 1        
         clock.tick(200)
